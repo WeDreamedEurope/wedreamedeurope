174d5de85a8922c6c19d93eb99376b1db8b04a9a
euro dream <wedreamedeurope@gmail.com>
2025-03-03 03:10:58 +0400

    PSA Area Looks Better

--- components/form/UploadPSA.comp.tsx
+++ components/form/UploadPSA.comp.tsx
@@ -1,14 +1,20 @@
+import { AlertCircle } from "lucide-react";
+
 export default function UploadPSA() {
   return (
-    <div className="flex flex-col gap-2 bg-yellow-500/10 text-sm leading-relaxed p-4 rounded-md text-yellow-100">
-      <div>
-        1. მესენჯერებში (Facebook, Whatsapp, Viber) მიღებულ ფოტოებს ჩამოჭრილი
-        აქვთ EXIF ინფორმაცია 
-      </div>
-      <div>
-        1. iPhone-ის ახალი მოდელები ფოტოებს იღებს სპეციფიკურ ფორმატში რაც
-        შეუძლებელს ხდის EXIF ინფორმაციის ამოღებას
-      </div>
+    <div className="flex flex-col gap-2  text-sm leading-relaxed p-4 rounded-md text-yellow-100">
+      <div className=" bg-opacity-50 border border-yellow-700 rounded-lg p-4">
+            <div className="flex">
+              <AlertCircle className="h-5 w-5 text-yellow-500 mr-2 flex-shrink-0 mt-0.5" />
+              <p>მესენჯერებიდან (Facebook, Whatsapp, Viber) მიღებულ ფოტოებს ჩამოშორებული აქვთ EXIF ინფორმაცია</p>
+            </div>
+          </div>
+          <div className=" bg-opacity-50 border border-yellow-700 rounded-lg p-4">
+            <div className="flex">
+              <AlertCircle className="h-5 w-5 text-yellow-500 mr-2 flex-shrink-0 mt-0.5" />
+              <p>iPhone-ის უახლესი მოდელები ფოტოებს იღებს სპეციფიკურ ფორმატში რაც EXIF ინფორმაციის ამოღებას შეუძლებელს ხდის</p>
+            </div>
+          </div>
     </div>
   );
 }
