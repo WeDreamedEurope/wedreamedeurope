ec26ec73757b92a010aedf2780652a1af2620b5d
euro dream <wedreamedeurope@gmail.com>
2025-03-03 15:08:21 +0400

    PopOver In Progress

--- components/Header.comp.tsx
+++ components/Header.comp.tsx
@@ -1,24 +1,46 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./ui/button";
+import Image from "next/image";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 export default function Header() {
   const { data: session } = useSession();
+
   return (
     <header className=" h-header-height flex-none flex px-4 items-center justify-between">
       <div>We Dreamed Europe</div>
       {session?.user ? (
-        <Button
-          onClick={() =>
-            signOut({
-              redirect: true,
-            })
-          }
-        >
-          Log Out Baby
-        </Button>
+        <UserExists profileUrl={session.user.image!} />
       ) : (
-        <Button onClick={() => signIn("google")}>Log In Baby</Button>
+        <button
+          className="text-white font-semibold"
+          onClick={() => signIn("google")}
+        >
+          შესვლა
+        </button>
       )}
     </header>
   );
 }
+
+const UserExists = ({ profileUrl }: { profileUrl: string }) => {
+  return (
+    <>
+      <Popover>
+        <PopoverTrigger asChild>
+          <button
+            // onClick={() => signOut()}
+            className="w-8 h-8 rounded-full overflow-hidden relative"
+          >
+            <Image src={profileUrl} alt="profile" fill />
+          </button>
+        </PopoverTrigger>
+        <PopoverContent asChild className="bg-black border-none min-w-[200px] text-gray-200" align="end">
+          <div className="flex flex-col gap-2 bg-black">
+            Lorem ipsum dolor sit amet.
+          </div>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+};
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,39 @@
+import { Button } from "@/components/ui/button";
 import { withAuth } from "@/components/WithAuth.com";
-import Link from "next/link";
-import React from "react";
-
+import testImage from "@/public/someimage.jpg";
+import { Trash } from "lucide-react";
+import Image from "next/image";
 const Profile = () => {
   return (
-    <div>
-      <Link href="/upload">Super Secret Component</Link>
+    <div className="flex flex-col   mt-8 w-full  sm:mx-auto max-w-5xl ">
+      <section className="flex flex-col bg-green-500/10 p-4 rounded-lg ">
+        <section className="flex items-center justify-between mb-4">
+          <h1 className="text-base font-semibold ">ბოლოს ატვირთული ფოტოები</h1>
+          <Button size={"sm"} variant={'destructive'}>ფოტოების წაშლა</Button>
+        </section>
+        <div className="grid grid-cols-4 gap-2">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div
+              key={index}
+              className="w-full aspect-video object-cover relative"
+            >
+              <div className="absolute top-2 right-2  flex items-center justify-center w-4 h-4 p-4 bg-red-500/50 backdrop-blur-sm rounded-full z-50">
+                <button>
+                  <Trash size={16} />
+                </button>
+              </div>
+              <Image
+                fill
+                src={
+                  "https://images.unsplash.com/photo-1513894697031-3e3aa4c54326?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                }
+                alt="test"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
