f0a46f1dcd593146afb242902d276db2615a0345
euro dream <wedreamedeurope@gmail.com>
2025-03-08 03:15:31 +0400

    mend

--- components/map/Sidebar/Sidebar.comp.tsx
+++ components/map/Sidebar/Sidebar.comp.tsx
@@ -1,10 +1,7 @@
-import { Photo_Location_Client } from "@/server/gis_query";
-import { useDateTimeContext } from "@/context/DateTimeContext";
-import { useMapContext } from "@/context/MapContenxt";
-import Sidebartutorial from "./PSA.comp";
-import SidebarGallery from "./SidebarGallery.comp";
 import DateAndTimeForm from "@/components/form/FormHeader";
 import usePhotoLoader from "@/hooks/usePhotoloader.hook";
+import Sidebartutorial from "./PSA.comp";
+import SidebarGallery from "./SidebarGallery.comp";
 // type Props = {
 //   photos: Photo_Location_Client[];
 // };
@@ -17,9 +14,7 @@ export default function MapSidebar() {
       <header className="w-full    pointer-events-auto bg-gray-800 sticky top-0 z-50 ">
         <DateAndTimeForm />
       </header>
-      <div
-      onClick={()=> tempHandlerOrWhatever()}
-      >{stateOfAction}</div>
+      <div onClick={() => tempHandlerOrWhatever()}>{stateOfAction}</div>
       {stateOfAction === "loaded" ? (
         <SidebarGallery />
       ) : (
