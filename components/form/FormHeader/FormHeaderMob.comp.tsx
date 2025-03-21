40f52e2bf622023419f0222da0173732ec80cd5b
euro dream <wedreamedeurope@gmail.com>
2025-03-22 03:40:37 +0400

    Removed debug text

--- components/form/FormHeader/FormHeaderMob.comp.tsx
+++ components/form/FormHeader/FormHeaderMob.comp.tsx
@@ -189,7 +189,7 @@ export default function FormHeaderMob({
           variant={"secondary"}
         >
 
-          მოძებნე {stateOfAction}
+          მოძებნე 
         </Button>
       </div>
     </div>
